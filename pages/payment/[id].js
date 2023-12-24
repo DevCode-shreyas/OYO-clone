fb10a1e91a4b5b8b350c050650a315cb27dd1474
shreyas bole <shreyasbole4@gmail.com>
2023-12-24 15:20:03 +0530

    project done

---
+++ pages/payment/[id].js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import Script from "next/script";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+const Payment = () => {
+  const router = useRouter();
+
+  const makePayment = async () => {
+    const val = {
+      id: router.query?.id,
+    };
+    const { data } = await axios.post(`/api/razorpay`, val);
+
+    const options = {
+      key: process.env.RAZORPAY_KEY,
+      name: "Aditya",
+      currency: data.currency,
+      amount: data.amount,
+      order_id: data.id,
+      description: "Thank You !",
+      handler: function (response) {},
+      prefill: {
+        name: "Shreyas",
+        email: "shreyasbole.3@gmail.com",
+        contact: +918652829404,
+      },
+    };
+
+    const paymentObj = new window.Razorpay(options);
+    paymentObj.open();
+  };
+
+  useEffect(() => {
+    makePayment();
+  }, []);
+
+  return (
+    <>
+      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
+    </>
+  );
+};
+
+export default Payment;
